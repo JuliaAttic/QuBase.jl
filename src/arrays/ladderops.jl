0dc1e40d9bcc8c5c9ca9dc35b124613b0a75919e
amitjamadagni <bitsjamadagni@gmail.com>
2015-04-02 15:32:17 +0530

    Spin Operators.

--- src/arrays/ladderops.jl
+++ src/arrays/ladderops.jl
@@ -37,38 +37,27 @@ function momentumop(n::Int)
     return scale(im/sqrt(2.), cop-cop')
 end
 
-function spin_h1(j)
+function spin_h1(j::Float64)
     m = reverse([-j:j-1])
     N = length(m)+1
     m_coeffs = [sqrt(j*(j+1.0) - (x+1.0)*x) for x in m]
     return spdiagm(m_coeffs,1,N,N)
 end
 
-function spin_h2(j)
+function spin_h2(j::Float64)
     m = reverse([-j:j])
     N = length(m)
     return spdiagm(m,0,N,N)
 end
 
-function spin_jx(j)
-    return QuArray(0.5*(spin_h1(j)+spin_h1(j)'))
-end
-
-function spin_jy(j)
-    return QuArray(-0.5*im*(spin_h1(j)-spin_h1(j)'))
-end
-
-function spin_jz(j)
-    return QuArray(spin_h2(j))
-end
-
-function spin_jp(j)
-    return QuArray(spin_h1(j))
-end
-
-function spin_jm(j)
-    return QuArray(spin_h1(j)')
-end
+spin_jx(j::Float64) = QuArray(0.5*(spin_h1(j)+spin_h1(j)'))
+spin_jy(j::Float64) = QuArray(-0.5*im*(spin_h1(j)-spin_h1(j)'))
+spin_jz(j::Float64) = QuArray(spin_h2(j))
+spin_jp(j::Float64) = QuArray(spin_h1(j))
+spin_jm(j::Float64) = QuArray(spin_h1(j)')
+sigmax = 2.0 * spin_jx(0.5)
+sigmay = 2.0 * spin_jy(0.5)
+sigmaz = 2.0 * spin_jz(0.5)
 
 export raiseop,
     lowerop,
@@ -78,5 +67,8 @@ export raiseop,
     spin_jy,
     spin_jz,
     spin_jp,
-    spin_jm
+    spin_jm,
+    sigmax,
+    sigmay,
+    sigmaz
 
