3197fa5a3ec19d5392ae655cd2a8ad78b7579a5f
Jarrett Revels <jarrettrevels@gmail.com>
2015-04-24 13:49:59 -0400

    spin code refactor for fewer tmp arrays and type stability

--- src/arrays/spinops.jl
+++ src/arrays/spinops.jl
@@ -1,71 +1,80 @@
 #########################
 # Spin Operator Methods #
 #########################
-
-immutable HalfSpin{S}
-    function HalfSpin()
-        S >= 0 ? new() : error("Spin must be positive")
-    end
+immutable HalfSpin{T<:Integer}
+    val::T
+    HalfSpin(val) = val >= 0 ? new(val) : error("Spin must be positive")
 end
 
-# The `spin` function takes the `j` argument from the value
-# domain to the type domain, making the `spin` function type unstable.
+HalfSpin{T}(val::T) = HalfSpin{T}(val)
 
-spin(j::Integer) = HalfSpin{2*j}()
-
-function spin(j::Float64)
+spin(s::HalfSpin) = s
+spin(j::Integer) = HalfSpin(2*j)
+function spin(j)
     if isinteger(j)       
         return spin(int(j)) # j = 0.0, 1.0, 2.0...
     elseif isinteger(2*j) 
-        return HalfSpin{int(2*j)}() # j = .5, 1.5, 2.5...
+        return HalfSpin(int(2*j)) # j = .5, 1.5, 2.5...
     else
         error("Spin must be a multiple of 1/2.")
     end
 end
 
-spin_value{S}(::HalfSpin{S}) = S/2
+spin_value(s::HalfSpin) = s.val/2
 
 #####################
 # Auxiliary Methods #
 #####################
+spin_coeffs(j, scalar) = [scalar*sqrt(j*(j+1)-(x+1)*x) for x in j-1:-1:-j]
 
-function mat_coeffs_1(j::Float64)
-    m = [j-1:-1:-j]
-    N = length(m)+1
-    m_coeffs = [sqrt(j*(j+1.0) - (x+1.0)*x) for x in m]
-    return spdiagm(m_coeffs,1,N,N)
+function spin_jxy_mat(s::HalfSpin, x_or_y)
+    N = s.val + 1
+    j = spin_value(s)
+
+    if x_or_y == :x
+        scalar = .5
+    elseif x_or_y == :y
+        scalar = -.5im
+    else
+        error("spin_jxy_mat can only construct matrices for :x or :y")
+    end
+
+    up_coeffs = spin_coeffs(j, scalar)
+    down_coeffs = x_or_y == :x ? up_coeffs : -1*up_coeffs
+    return spdiagm((up_coeffs, down_coeffs), (1, -1), N, N)
 end
 
-function mat_coeffs_2(j::Float64)
-    m = [j:-1:-j]
-    N = length(m)
-    return spdiagm(m,0,N,N)
+function spin_jpm_mat(s::HalfSpin, p_or_m)
+    N = s.val + 1
+    
+    if p_or_m == :p
+        k = 1
+    elseif p_or_m == :m
+        k = -1 
+    else
+        error("spin_jpm_mat can only construct matrices for :p or :m")
+    end
+    
+    return spdiagm(spin_coeffs(spin_value(s), 1), k, N, N)
 end
 
-function spin_h1(k::HalfSpin)
-    j = spin_value(k)
-    return mat_coeffs_1(j)
-end
-
-function spin_h2(k::HalfSpin)
-    j = spin_value(k)
-    return mat_coeffs_2(j)
+function spin_jz_mat(s::HalfSpin)
+    j = spin_value(s)
+    return spdiagm(j:-1:-j)
 end
 
 ################################
 # Jx, Jy, Jz, Jp, Jm operators #
 ################################
-
-spin_Jx(j) = QuArray(0.5*(spin_h1(spin(j))+spin_h1(spin(j))'))
-spin_Jy(j) = QuArray(-0.5*im*(spin_h1(spin(j))-spin_h1(spin(j))'))
-spin_Jz(j) = QuArray(spin_h2(spin(j)))
-spin_Jp(j) = QuArray(spin_h1(spin(j)))
-spin_Jm(j) = QuArray(spin_h1(spin(j))')
+spin_Jx(j) = QuArray(spin_jxy_mat(spin(j), :x))
+spin_Jy(j) = QuArray(spin_jxy_mat(spin(j), :y))
+spin_Jz(j) = QuArray(spin_jz_mat(spin(j)))
+spin_Jp(j) = QuArray(spin_jpm_mat(spin(j), :p))
+spin_Jm(j) = QuArray(spin_jpm_mat(spin(j), :m))
 
 ############################
 #  Pauli spin 1/2 matrices #
 ############################
-
 const sigma_x = 2.0 * spin_Jx(1/2)
 const sigma_y = 2.0 * spin_Jy(1/2)
 const sigma_z = 2.0 * spin_Jz(1/2)
