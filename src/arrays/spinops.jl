5e0303f590b0c581fdf741b2fa632397d273b926
Jarrett Revels <jarrettrevels@gmail.com>
2015-04-27 14:22:24 -0400

    rm'd underscores and caps from spin function names

--- src/arrays/spinops.jl
+++ src/arrays/spinops.jl
@@ -27,7 +27,7 @@ spin_value(s::HalfSpin) = s.val/2
 #####################
 spin_coeffs(j, scalar) = [scalar*sqrt(j*(j+1)-(x+1)*x) for x in j-1:-1:-j]
 
-function spin_jyx_mat(s::HalfSpin, a, b)
+function spinjyx_mat(s::HalfSpin, a, b)
     N = s.val + 1
     j = spin_value(s)
     up_coeffs = spin_coeffs(j, a)
@@ -35,52 +35,56 @@ function spin_jyx_mat(s::HalfSpin, a, b)
     return spdiagm((up_coeffs, down_coeffs), (1, -1), N, N)
 end
 
-spin_jx_mat(j) = spin_jyx_mat(spin(j), .5, 1)
-spin_jy_mat(j) = spin_jyx_mat(spin(j), -.5im, -1)
+spinjx_mat(j) = spinjyx_mat(spin(j), .5, 1)
+spinjy_mat(j) = spinjyx_mat(spin(j), -.5im, -1)
 
-function spin_jpm_mat(s::HalfSpin, k)
+function spinjpm_mat(s::HalfSpin, k)
     N = s.val + 1
     return spdiagm(spin_coeffs(spin_value(s), 1), k, N, N)
 end
 
-spin_jp_mat(j) = spin_jpm_mat(spin(j), 1)
-spin_jm_mat(j) = spin_jpm_mat(spin(j), -1)
+spinjp_mat(j) = spinjpm_mat(spin(j), 1)
+spinjm_mat(j) = spinjpm_mat(spin(j), -1)
 
-function spin_jz_mat(s::HalfSpin)
+function spinjz_mat(s::HalfSpin)
     j = spin_value(s)
     return spdiagm(j:-1:-j)
 end
 
-spin_jz_mat(j) = spin_jz_mat(spin(j))
+spinjz_mat(j) = spinjz_mat(spin(j))
 
 ################################
 # Jx, Jy, Jz, Jp, Jm operators #
 ################################
-# This Wikipedia pages give a nice overview of spin/ladder operators:
+# These Wikipedia pages give a nice overview of spin/ladder operators:
 # http://en.wikipedia.org/wiki/Spin_%28physics%29#Operator
 # http://en.wikipedia.org/wiki/Ladder_operator#Angular_momentum
-spin_Jx(j) = QuArray(spin_jx_mat(j))
-spin_Jy(j) = QuArray(spin_jy_mat(j))
-spin_Jz(j) = QuArray(spin_jz_mat(j))
-spin_Jp(j) = QuArray(spin_jp_mat(j))
-spin_Jm(j) = QuArray(spin_jm_mat(j))
+spinjx(j) = QuArray(spinjx_mat(j))
+spinjy(j) = QuArray(spinjy_mat(j))
+spinjz(j) = QuArray(spinjz_mat(j))
+spinjp(j) = QuArray(spinjp_mat(j))
+spinjm(j) = QuArray(spinjm_mat(j))
 
 ############################
 #  Pauli spin 1/2 matrices #
 ############################
-const sigma_x = 2.0 * spin_Jx(1/2)
-const sigma_y = 2.0 * spin_Jy(1/2)
-const sigma_z = 2.0 * spin_Jz(1/2)
+const sigmax = 2.0 * spinjx(1/2)
+const sigmay = 2.0 * spinjy(1/2)
+const sigmaz = 2.0 * spinjz(1/2)
+const sigmap = sigmax + im * sigmay
+const sigmam = sigmax - im * sigmay
 
-# TODO : unicode sigma_y, sigma_z
-const σₓ = sigma_x
+# TODO : unicode sigmay, sigmaz
+const σₓ = sigmax
 
-export spin_Jx,
-    spin_Jy,
-    spin_Jz,
-    spin_Jp,
-    spin_Jm,
-    sigma_x,
-    sigma_y,
-    sigma_z,
+export spinjx,
+    spinjy,
+    spinjz,
+    spinjp,
+    spinjm,
+    sigmax,
+    sigmay,
+    sigmaz,
+    sigmap,
+    sigmam,
     σₓ
