0c904e939e40f465e90cd50c3c1ab1df5ee47a00
amitjamadagni <bitsjamadagni@gmail.com>
2015-07-12 18:55:40 +0800

    Complex coeffs support.

--- src/arrays/arraymath.jl
+++ src/arrays/arraymath.jl
@@ -121,6 +121,9 @@ function \{B<:OrthonormalBasis}(op::AbstractQuMatrix{B}, vec::AbstractQuVector{B
     return QAT(div, bases(op,1))
 end
 
+# convert coeffs to complex
+Base.complex{B<:OrthonormalBasis}(qarr::AbstractQuArray{B}) = similar_type(qarr)(complex(coeffs(vec(qarr))), bases(qarr))
+
 #  Vectorize QuArray
 Base.vec{B<:OrthonormalBasis}(vec1::AbstractQuArray{B}) = QuArray(vec(coeffs(vec1)))
 
